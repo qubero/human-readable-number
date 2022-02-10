35eac43b529c91e10fe4cf7eb2e31ea3e2ffa6fb
wolsos-v <mamaev757@gmail.com>
2022-02-10 03:55:27 +0300

    feat: implement toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    const words = [
+        [
+            'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+            'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+        ],
+        [
+            'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+        ]
+    ];
+
+    const createTens = (number) => {
+        if (number < 20) {
+            return words[0][number];
+        }
+
+        const numFloor = Math.floor(number / 10);
+        const numReminder = number % 10;
+
+        const ten = words[1][numFloor - 2];
+        const digit = numReminder ? words[0][numReminder] : ``;
+
+        return [ten, digit].join(` `).trim();
+    };
+
+    if (number < 100) {
+        return createTens(number);
+    } else {
+        const numFloor = Math.floor(number / 100);
+        const numReminder = number % 100;
+
+        let hundred = numFloor ? `${words[0][numFloor]} hundred` : ``;
+        let tens = numReminder ? createTens(numReminder) : ``;
+
+        return [hundred, tens].join(` `).trim();
+    }
 }
